8850f88409f663d970e3bbf61ca2b53df3df4c5e
Camran Qadeer <camqadeer@gmail.com>
2022-03-12 13:36:58 +0000

    Update Beach Tileset.tsx

--- core/assets/Beach Tileset.tsx
+++ core/assets/Beach Tileset.tsx
@@ -1,9 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="fixed" tilewidth="32" tileheight="32" spacing="4" margin="2" tilecount="111" columns="37">
  <image source="Beach Tileset.png" width="1332" height="108"/>
- <tile id="0">
-  <objectgroup draworder="index" id="3">
-   <object id="13" x="2" y="4" width="24" height="25"/>
-  </objectgroup>
- </tile>
 </tileset>
