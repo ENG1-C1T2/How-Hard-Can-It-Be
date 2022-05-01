6e7ecacd92fc2f7564031720b4a282c9a51dc8c8
Kieron Jones <94188083+KieronJones@users.noreply.github.com>
2022-05-01 13:03:50 +0100

    Removed ConstantTests

---
+++ tests/src/com/mygdx/tests/game/ShipTests.java
@@ -0,0 +1,17 @@
+package com.mygdx.tests.game;
+
+
+import com.mygdx.game.Components.Pirate;
+import com.mygdx.game.Entitys.Ship;
+import com.mygdx.tests.GdxTestRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(GdxTestRunner.class)
+
+public class ShipTests {
+
+}
